a2beb9e56f4e2cf59d6b6f4d14f29141e806fd3c
mollymoon2b <mollymoon2b@gmail.com>
2014-11-17 14:13:18 +0100

    get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,70 +6,56 @@
 /*   By: ade-bonn <ade-bonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 15:47:59 by ade-bonn          #+#    #+#             */
-/*   Updated: 2014/11/16 15:47:59 by ade-bonn         ###   ########.fr       */
+/*   Updated: 2014/11/17 14:05:57 by ade-bonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	my_buf(char **buf, int *i, char **line)
-{
-	while (*buf && (*buf)[(*i)])
-	{
-		if ((*buf)[(*i)] == '\n')
-		{
-			*line = ft_strjoin(*line, *buf, (*i));
-			(*i)++;
-			*buf = &(*buf)[(*i)];
-			return (1);
-		}
-		(*i)++;
-	}
-	return (0);
-}
-
-int	my_read(int const fd, char **buf)
-{
-	int		ret;
-
-	ret = 0;
-	*buf = (char *)malloc((sizeof(char) * BUFF_SIZE) + 1);
-	if (*buf == NULL)
-		return (-1);
-	ret = read(fd, *buf, BUFF_SIZE);
-	return (ret);
-}
-
 int	get_next_line(int fd, char **line)
 {
-	static	char 	*buff; 
+	char			buff[BUFF_SIZE + 1];
 	int				ret;
-	char			*tmp;
+	static char		*tmp;
+	int				len;
+	char *tmp2;
 
-	if(!buff)
-		buff = ft_strnew(BUFF_SIZE + 1);
-	while (ret = my_read(fd, line, BUFF_SIZE) && ft_strchr(line, '\n') == NULL) 
+	if (!tmp)
+		tmp = ft_strnew(0);
+	ret = 0;
+	while ((ft_strchr(tmp, '\n')) == NULL
+		&& (ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		if (ft_buf(&buf, &i, line) == 1)
-			return (1);
-		*line = tmp;
-		while (ft_strchr(line, '\n'))
-			i++;
-		tmp = ft_strjoin(*line, buf, i);
-		ret = my_read(fd, &line);
-		buff[ret] = '\n';
+		buff[ret] = '\0';
+		tmp2 = tmp;
+		tmp = ft_strjoin(tmp, buff);
+		ft_strdel(&tmp2);
 	}
-	return (ret);
+	if (ret == -1)
+		return (-1);
+	if (ret == 0 && ft_strchr(tmp, '\n') == NULL)
+	{
+		*line = tmp;
+		tmp == NULL;
+		return (0);
+	}
+	len = ft_strchr(tmp, '\n') - tmp;
+	*line = ft_strsub(tmp, 0, len);
+	tmp2 = tmp;
+	tmp = ft_strchr(tmp, '\n') + 1;
+	ft_strdel(&tmp2);
+	return (1);
 }
-		
-int  main (int ac, char **av)
-{
-	int ret;
-	char *line;
-	int fd
 
-	fd = open(argv[1], O_RDONLY);
-		while (get_next_line(fd, &line) > 0)
+int	main(int ac, char **av)
+{
+	int		ret;
+	char	*line;
+	int		fd;
+	int		i;
+
+	fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
 	{
 		ft_putendl(line);
 		free(line);
--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,29 @@
-#ifndef GET-NEXT-LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-bonn <ade-bonn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/17 10:48:27 by ade-bonn          #+#    #+#             */
+/*   Updated: 2014/11/17 14:00:19 by ade-bonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 
+# define BUFF_SIZE 32
 # include <string.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <sys/uio.h>
 # include <sys/types.h>
+# include "libft/libft.h"
 
-int	get_next_line(int fd, char **line);
-int	my_read(int const fd, char **buf);
-int	my_buf(char **buf, int *i, char **line);
+int		get_next_line(int fd, char **line);
+int		my_read(int const fd, char **buf);
+int		my_buf(char **buf, int *i, char **line);
 char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
