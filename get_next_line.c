ebe5191f40bb8b4fc12cc981b8c83fa685082809
mollymoon2b <mollymoon2b@gmail.com>
2014-11-17 15:47:33 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ade-bonn <ade-bonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 15:47:59 by ade-bonn          #+#    #+#             */
-/*   Updated: 2014/11/17 14:05:57 by ade-bonn         ###   ########.fr       */
+/*   Updated: 2014/11/17 15:46:41 by ade-bonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	get_next_line(int fd, char **line)
 	int				ret;
 	static char		*tmp;
 	int				len;
-	char *tmp2;
+	char			*tmp2;
 
 	if (!tmp)
 		tmp = ft_strnew(0);
@@ -27,10 +27,11 @@ int	get_next_line(int fd, char **line)
 		&& (ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		tmp2 = tmp;
+		//	tmp2 = tmp;
 		tmp = ft_strjoin(tmp, buff);
-		ft_strdel(&tmp2);
+		//ft_strdel(&tmp2);
 	}
+
 	if (ret == -1)
 		return (-1);
 	if (ret == 0 && ft_strchr(tmp, '\n') == NULL)
@@ -39,11 +40,12 @@ int	get_next_line(int fd, char **line)
 		tmp == NULL;
 		return (0);
 	}
+
 	len = ft_strchr(tmp, '\n') - tmp;
 	*line = ft_strsub(tmp, 0, len);
-	tmp2 = tmp;
+	//tmp2 = tmp;
 	tmp = ft_strchr(tmp, '\n') + 1;
-	ft_strdel(&tmp2);
+//	ft_strdel(&tmp2);
 	return (1);
 }
 
